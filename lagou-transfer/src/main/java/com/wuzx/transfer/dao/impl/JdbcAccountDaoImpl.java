04b4bbff3836e24abc2dafe8e8a57011c5da38b6
wuzhixuan <jackwu@java.com>
2021-11-04 00:45:05 +0800

    Initial commit

---
+++ lagou-transfer/src/main/java/com/wuzx/transfer/dao/impl/JdbcAccountDaoImpl.java
@@ -0,0 +1,57 @@
+package com.wuzx.transfer.dao.impl;
+
+
+
+import com.wuzx.transfer.dao.AccountDao;
+import com.wuzx.transfer.pojo.Account;
+import com.wuzx.transfer.utils.DruidUtils;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+/**
+ * @author 应癫
+ */
+public class JdbcAccountDaoImpl implements AccountDao {
+
+
+    @Override
+    public Account queryAccountByCardNo(String cardNo) throws Exception {
+        //从连接池获取连接
+        Connection con = DruidUtils.getInstance().getConnection();
+        String sql = "select * from account where cardNo=?";
+        PreparedStatement preparedStatement = con.prepareStatement(sql);
+        preparedStatement.setString(1,cardNo);
+        ResultSet resultSet = preparedStatement.executeQuery();
+
+        Account account = new Account();
+        while(resultSet.next()) {
+            account.setCardNo(resultSet.getString("cardNo"));
+            account.setName(resultSet.getString("name"));
+            account.setMoney(resultSet.getInt("money"));
+        }
+
+        resultSet.close();
+        preparedStatement.close();
+        con.close();
+
+        return account;
+    }
+
+    @Override
+    public int updateAccountByCardNo(Account account) throws Exception {
+
+        // 从连接池获取连接
+        Connection con = DruidUtils.getInstance().getConnection();
+        String sql = "update account set money=? where cardNo=?";
+        PreparedStatement preparedStatement = con.prepareStatement(sql);
+        preparedStatement.setInt(1,account.getMoney());
+        preparedStatement.setString(2,account.getCardNo());
+        int i = preparedStatement.executeUpdate();
+
+        preparedStatement.close();
+        con.close();
+        return i;
+    }
+}
